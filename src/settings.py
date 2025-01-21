c32815708b2251f1568b4d397160aaf22b322f89
khasan <khasanknyazev81@gmail.com>
2025-01-21 14:14:16 +0300

    FIX settings .env

--- src/settings.py
+++ src/settings.py
@@ -1,16 +1,10 @@
 import os
 import base64
 
-from pydantic_settings import BaseSettings
-
-def generate_random_base64_string(length: int) -> str:
-    random_bytes = os.urandom(length)
-    base64_string = base64.b64encode(random_bytes).decode('utf-8')
-    return base64_string
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    SECRET_DOCS_PATH: str = generate_random_base64_string(10)
     BASE64_SECRET_CODE_KEY: str | None
     SECRET_KEY: str | None
 
@@ -18,6 +12,7 @@ class Settings(BaseSettings):
     def SECRET_CODE_KEY(cls) -> bytes:
         return bytes(cls.BASE64_CODE_KEY, "utf-8")
 
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
 
 settings = Settings()
-
