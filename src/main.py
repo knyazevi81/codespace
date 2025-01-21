24124179aba2d4cf1a52a44a1ef9e10151ed630a
khasan <khasanknyazev81@gmail.com>
2025-01-21 14:15:24 +0300

    add users router

--- src/main.py
+++ src/main.py
@@ -4,31 +4,35 @@ from contextlib import asynccontextmanager
 
 from src.database import create_tables
 from src.codespace.router import router as code_space_router
+from src.users.router import router as users_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await create_tables()
-    print("Готово")
+    await create_tables()  # Создаем Базу
     yield
 
 
 def get_application() -> FastAPI:
-    _app = FastAPI(
-        title="savecode api",
-        lifespan=lifespan
-    )
+    _app = FastAPI(title="savecode api", lifespan=lifespan)
 
     _app.include_router(code_space_router)
+    _app.include_router(users_router)
 
     _app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"], #TODO add front
+        allow_origins=["*"],  # TODO add front
         allow_credentials=True,
         allow_methods=["GET", "POST", "DELETE", "PUT", "PATCH"],
-        allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
-                       "Access-Control-Allow-Origin", "Authorization"],
+        allow_headers=[
+            "Content-Type",
+            "Set-Cookie",
+            "Access-Control-Allow-Headers",
+            "Access-Control-Allow-Origin",
+            "Authorization",
+        ],
     )
     return _app
+
 
 app = get_application()
